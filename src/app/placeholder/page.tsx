67badc6f675128bfff450501da5c73075bd7b4fe
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-10-24 10:05:24 -0600

    Tarea

--- src/app/placeholder/page.tsx
+++ src/app/placeholder/page.tsx
@@ -12,14 +12,23 @@ async function Placeholder() {
   }
 
   return (
-    <div>
-      {users.map((usuarios: User) => (
-        <div key={usuarios.id} className="mt-8">
-          <h1 className="bg-yellow-300">{usuarios.name}</h1>
-          <p>{usuarios.username}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <div>
+        {users.map((usuarios: User) => (
+          <div key={usuarios.id} className="mt-8">
+            <h1 className="bg-yellow-300">{usuarios.name}</h1>
+            <p>{usuarios.username}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h4>TAREA</h4>
+        <p>
+          Darle una UI correcta a los usuarios e implemtar las demás propiedades
+          del objeto (ej. email, address, phone, website, etc)
+        </p>
+      </div>
+    </>
   );
 }
 
