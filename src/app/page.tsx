84d5684bc34744074504df888677b5c7af4b5481
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-10-24 09:56:51 -0600

    Actualización

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 export default async function Page() {
   const data = await fetch("https://api.vercel.app/blog");
   const posts = await data.json();
+  console.log(posts);
 
   //Declaramos una interfaz de los resultados que trae nuesta api
   interface Post {
     id: string;
     title: string;
+    content: string;
   }
 
   return (
     <div>
       {posts.map((post: Post) => (
-        <div key={post.id}>{post.title}</div>
+        <div key={post.id} className="mt-8">
+          <h1 className="bg-yellow-300">{post.title}</h1>
+          <p>{post.content}</p>
+        </div>
       ))}
     </div>
   );
