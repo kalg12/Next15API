3d8942571397254f17aa86bb9351c9f43c7eacd4
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-10-24 09:42:55 -0600

    Actualización

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,18 @@
-import { GetStaticProps } from "next";
+export default async function Page() {
+  const data = await fetch("https://api.vercel.app/blog");
+  const posts = await data.json();
 
-/* Es importante declarar interfaces por seguridad en Typescript */
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
+  //Declaramos una interfaz de los resultados que trae nuesta api
+  interface Post {
+    id: string;
+    title: string;
+  }
 
-/* The `interface HomeProps` is defining the structure of the props that the `Home` component expects
-to receive. In this case, it specifies that the `Home` component expects a prop named `users` which
-should be an array of objects that adhere to the `User` interface. This helps ensure type safety and
-provides clarity on the expected shape of the props that should be passed to the `Home` component. */
-interface HomeProps {
-  users: User[];
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: User[] = await respuesta.json();
-
-  return {
-    props: {
-      users,
-    },
-  };
-};
-
-export default function Home({ users }: HomeProps) {
   return (
-    <>
-      <h1>Proyecto limpio</h1>
-    </>
+    <div>
+      {posts.map((post: Post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
   );
 }
